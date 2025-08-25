72a396af56f43d37c4cfe6309d265f0aa2d03973
simonhayes51 <simonhayes_@hotmail.com>
2025-08-25 01:11:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,4 @@
 import os
-import json
-import asyncpg
-import aiohttp
-import csv
-import io
-from fastapi import FastAPI, Request, HTTPException, Depends, UploadFile, File
-from fastapi.responses import RedirectResponse, StreamingResponse
-from fastapi.middleware.cors import CORSMiddleware
-from starlette.middleware.sessions import SessionMiddleware
-from dotenv import load_dotenv
-from contextlib import asynccontextmanager
-import logging
-import requests
-from bs4 import BeautifulSoup
 
 load_dotenv()
 
