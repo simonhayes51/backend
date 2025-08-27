9e11f90482c7e58f52f89a057039e95d8f123302
simonhayes51 <simonhayes_@hotmail.com>
2025-08-27 02:11:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -334,7 +334,7 @@ async def callback(request: Request):
     token_url = "https://discord.com/api/oauth2/token"
     data = {
         "client_id": DISCORD_CLIENT_ID,
-        "client_secret": DISCORD_CLIENT_SECRET",
+        "client_secret": DISCORD_CLIENT_SECRET,
         "grant_type": "authorization_code",
         "code": code,
         "redirect_uri": DISCORD_REDIRECT_URI,
